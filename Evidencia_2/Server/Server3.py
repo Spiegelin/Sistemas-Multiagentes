58c92836fe71982418aace91ce8120067aba564e
Diego Cisneros <madafanes@Laptop-de-Diego.local>
2024-09-05 16:28:39 -0600

    Get dron pos y camara

--- Evidencia_2/Server/Server3.py
+++ Evidencia_2/Server/Server3.py
@@ -1,60 +1,39 @@
-import sys
-import os
 import asyncio
+import sys, os
 import websockets
 import json
-
-# Añadir el directorio raíz al PYTHONPATH
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from Model import DronModel as model
-import UnityFunctions
+
+# Importar las funciones necesarias desde UnityFunctions
+from UnityFunctions.UnityFunctions import set_websocket_dron, set_websocket_camara, get_dron_position, get_camara_position
 
 async def handler(websocket, path):
     if path == "/camara":
+        print("Conexión establecida en la ruta /camara")
         try:
-            # Ejecutar la función para obtener la posición de la cámara
-            position = await UnityFunctions.get_camera_position()
-            # Enviar la posición de la cámara al cliente
-            response_message = json.dumps({"pos": position})
-            await websocket.send(response_message)
+            await set_websocket_camara(websocket)
+            async for message in websocket:
+                print(f"Mensaje recibido de la cámara: {message}")
 
+        except websockets.ConnectionClosed:
+            print("Conexión cerrada en /camara")
         except Exception as e:
             print(f"Error en /camara: {e}")
-
 
     elif path == "/dron":
         print("Conexión establecida en la ruta /dron")
         try:
+            await set_websocket_dron(websocket)
             async for message in websocket:
-                print(f"Mensaje recibido en /dron: {message}")
-
-                try:
-                    data = json.loads(message)
-                    action = data.get('action')
-                    position = data.get('position')
-
-                    if action == "move_to" and position:
-                        # Enviar comando de movimiento al dron
-                        move_message = f"muevete a {position}"
-                        await websocket.send(move_message)
-                        print(f"Comando de movimiento enviado: {move_message}")
-
-                    elif action is None:
-                        # Por defecto, solicita la posición del dron
-                        request_message = "Dame la posicion del dron"
-                        await websocket.send(request_message)
-                        print(f"Mensaje enviado para solicitar posición del dron: {request_message}")
-
-                except json.JSONDecodeError as e:
-                    print(f"Error de decodificación de JSON: {e}")
-                except Exception as e:
-                    print(f"Error al procesar el mensaje: {e}")
+                print(f"Mensaje recibido del dron: {message}")
 
         except websockets.ConnectionClosed:
             print("Conexión cerrada en /dron")
-
-    else:  # Manejar la ruta raíz
+        except Exception as e:
+            print(f"Error en /dron: {e}")
+    
+    else:
         print("Conexión establecida en la ruta raíz /")
         try:
             while True:
@@ -64,10 +43,14 @@ async def handler(websocket, path):
                 response = {"status": "connected to /"}
                 await websocket.send(json.dumps(response))
 
+                print("A punto de llamar al main")
+                await get_dron_position()
+                await get_camara_position()
+
         except websockets.ConnectionClosed:
             print("Conexión cerrada en /")
-        except Exception as e:
-            print(f"Error en la ruta raíz: {e}")
+        #except Exception as e:
+         #   print(f"Error en la ruta raíz: {e}")
 
 async def main():
     start_server = websockets.serve(handler, 'localhost', 8765)
